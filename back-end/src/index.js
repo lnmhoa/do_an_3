601f9c151ee2eb733a79c3b8931934f96a774ce7
lnmhoa <herozonce1@gmail.com>
2024-02-13 19:23:51 +0700

    ok

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const { default: mongoose } = require('mongoose');
 const router = require('./routes');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 dotenv.config();
 const app = express();
@@ -12,15 +12,15 @@ const port = process.env.PORT || 3003;
 app.use(cors());
 app.use(bodyParser.json());
 
-router(app)
+router(app);
 
 mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.listen(port);

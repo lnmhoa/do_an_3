defbe43c433012cafe86a3c97e97762a1c36978a
dat-nglt <dat.nglt@gmail.com>
2024-02-25 02:06:10 +0700

    header-top

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,12 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import GlobalStyle from './components/GlobalStyle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <GlobalStyle>
+                <App />
+            </GlobalStyle>
         </Provider>
     </React.StrictMode>,
 );

2f208bc46cf32c0d1309998626da8c87a1b3ed3c
dat-nglt <dat.nglt@gmail.com>
2024-03-25 22:31:56 +0700

    detail product

--- front-end/src/pages/DetailProduct/DetailProduct.jsx
+++ front-end/src/pages/DetailProduct/DetailProduct.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
-import { FaShippingFast, FaHandPointRight, FaCartPlus, FaCaretRight } from 'react-icons/fa';
+import { FaShippingFast, FaHandPointRight, FaCartPlus, FaCaretRight, FaSearch } from 'react-icons/fa';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 import styles from './DetailProduct.module.scss';
+import ProductSlider from '../../components/ProductSlider/ProductSlider';
 
 const cx = classNames.bind(styles);
 
@@ -39,6 +40,12 @@ const color = [
 ];
 
 function DetailProduct({ title }) {
+    const [checkFit, setCheckFit] = useState(true);
+
+    const handleFit = () => {
+        setCheckFit(false);
+    };
+
     useEffect(() => {
         document.title = title;
     }, [title]);
@@ -51,25 +58,29 @@ function DetailProduct({ title }) {
         <>
             <Breadcrumbs name={infoItem[0].name} />
             <div className={cx('container')}>
+                {/* Tên sản phẩm */}
                 <div className={cx('top')}>
                     <h1>{infoItem[0].name}</h1>
                 </div>
 
+                {/* Thông tin về sản phẩm bao gồm giá hình và thông số */}
                 <div className={cx('info-product')}>
+                    {/* Hình ảnh sản phẩm */}
                     <div className={cx('box-1')}>
-                        <img src={require('../../image/Upload/Product/product.jpg')} alt="" />
+                        <ProductSlider />
                     </div>
 
+                    {/* Giá và option mua hàng */}
                     <div className={cx('box-2')}>
                         <div className={cx('price')}>
-                            <span style={{ color: 'rgb(255, 71, 54)', fontSize: 25, fontWeight: 700 }}>
+                            <span style={{ color: 'rgb(255, 71, 54)', fontSize: 20, fontWeight: 700 }}>
                                 15.000.000 &#8363;
                             </span>
-                            <span style={{ fontSize: 15, textDecoration: 'line-through', fontStyle: 'italic' }}>
+                            <span style={{ fontSize: 14, textDecoration: 'line-through', fontStyle: 'italic' }}>
                                 20.000.000 &#8363;
                             </span>
                             <span> | </span>
-                            <span style={{ fontSize: 12 }}>Giá đã bao gồm thuế VAT</span>
+                            <span style={{ fontSize: 11 }}>Giá đã bao gồm thuế VAT</span>
                         </div>
 
                         <div className={cx('free')}>
@@ -190,71 +201,107 @@ function DetailProduct({ title }) {
                         </div>
                     </div>
 
-                    <div className={cx('box-3')}>
+                    {/* Thông số kỹ thuật của sản phamarF */}
+                    <div>
                         <div
                             style={{
                                 textAlign: 'center',
                                 border: '1px solid var(--color-1)',
-                                borderRadius: '5px',
-                                padding: '5px 0',
-                                fontWeight: 600,
-                                boxShadow: '1px 1px 3px varc(--color-1)',
+                                padding: '10px 0',
+                                fontWeight: 700,
+                                backgroundColor: 'var(--color-1)',
+                                color: '#fff',
                             }}
                         >
                             THÔNG SỐ KỸ THUẬT
                         </div>
-                        <h3>
-                            <center>{infoItem[0].name}</center>
-                        </h3>
-                        {
-                            <div className={cx('thong-so-ky-thuat')}>
-                                {Object.entries(parameters).map(([key, value]) => (
-                                    <div key={key}>
-                                        <p>
-                                            <strong>{key}:</strong> {value}
-                                        </p>
-                                    </div>
-                                ))}
-                            </div>
-                        }
+                        <div className={cx('box-3')}>
+                            <h3>
+                                <center>{infoItem[0].name}</center>
+                            </h3>
+                            {
+                                <div className={cx('thong-so-ky-thuat')}>
+                                    {Object.entries(parameters).map(([key, value]) => (
+                                        <div key={key}>
+                                            <p>
+                                                <strong>{key}:</strong> {value}
+                                            </p>
+                                        </div>
+                                    ))}
+                                </div>
+                            }
+                        </div>
                     </div>
                 </div>
-                <div className={cx('detail-info')}>
-                Galaxy S23 Ultra mang đến một thiết kế quen thuộc, nhìn thoáng qua, trông giống hệt với người tiền nhiệm
-                của nó: thiết kế Infinity-O Edge góc cạnh được làm cong về hai cạnh bên, mặt lưng hoàn thiện từ chất
-                liệu kính nhám mờ. Ngoài ra, thiết bị vẫn đi kèm bút S Pen quen thuộc và máy ảnh zoom quang 10X. Tuy
-                nhiên phiên bản S23 Ultra vẫn có những điểm khác biệt như viền 2 bên được vát phẳng hơn, kèm theo cụm
-                camera chính sẽ to hơn một chút với vòng kim loại bao quanh camera nhìn rõ nét hơn. Màn hình ít bo cong
-                hơn 30%, đem lại trải nghiệm dùng bút S Pen tốt hơn. Ngoài ra, phiên bản S23 Ultra nói riêng và dòng
-                Galaxy S23 nói chung có thiết kế đối xứng đậm chất Ultra và thân thiện với môi trường hơn, qua việc 12
-                linh kiện làm từ nguyên vật liệu tái chế bao gồm nhôm, thủy tinh tái chế tiền tiêu thụ và nhựa tổng hợp
-                từ lưới đánh cá, thùng nước và chai nhựa polyetylen terephthalate (PET) đã qua sử dụng. Thế hệ S23 cũng
-                là thiết bị di động đầu tiên sử dụng mặt kính Corning Gorilla Glass Victus 2 chống chịu tốt hơn khi rơi
-                trên các bề mặt cứng và thiết kế kết hợp linh kiện làm từ vật liệu tái chế tiền tiêu thụ lên tới 22%.
-                Bao bì của thiết bị cũng được làm 100% từ vật liệu tái chế. Galaxy S23 Ultra - Thiết kế hướng đến sự
-                phát triển bền vững Hệ thống camera siêu phân giải và bắt trọn mọi khoảnh khắc dù ngày hay đêm S23 Ultra
-                có màn hình QHD OLED 6,8 inch với độ phân giải 3088 x 1400 pixel và độ sáng tối đa 1.750 nits, tốc độ
-                làm mới 120Hz giống như trước đây, có thể giảm xuống 1Hz. Tuy nhiên màn hình mới đã được thiết kế sao
-                cho phẳng hơn, vừa đảm bảo khả năng trình chiếu vô cực lại khiến cho hai cạnh của máy được dày lên, giúp
-                sử dụng S Pen dễ dàng hơn. Galaxy S23 Ultra còn hỗ trợ chế độ chụp ảnh thiên văn sử dụng Hyperlapse. Hệ
-                thống camera chính gồm 5 ống kính kèm cảm biến lấy nét laser và đèn flash LED vẫn được đặt phía bên trái
-                điện thoại. Công nghệ AI xác định chủ thể (object-based AI) sẽ phân tích từng chi tiết trong khung hình,
-                thậm chí từng đặc điểm nhỏ trên khuôn mặt như tóc, mắt, để phản ánh chân thực đặc điểm của đối tượng
-                trong ảnh... Camera chính có độ phân giải tăng vọt lên 200MP cùng ống kính khẩu độ lớn F/1.7. Bộ ba
-                camera còn lại phía sau vẫn không thay đổi: một camera góc siêu rộng 12MP, cộng với một camera tele 3x
-                và một camera tele 10x, cả hai đều có độ phân giải 10MP. Camera selfie hiện cũng là 12MP sau khi giảm từ
-                40MP trong S22 Ultra. Ngoài ra, Galaxy S23 Ultra có thể điều chỉnh độ sáng một cách tự động suốt cả
-                ngày. Camera Mắt Thần Bóng Đêm (Night Ography Camera) mới giảm flare sáng hiệu quả trên S23 giúp bắt
-                được những điểm ảnh thu sáng và ổn định khung hình video. Sâu hơn, Galaxy S23 Ultra giúp bắt được những
-                khung hình và thước phim sắc nét nhờ việc được xây dựng với điểm ảnh 2,4um – lớn nhất từ ​​trước đến nay
-                của Samsung, cho phép các ống kính máy ảnh thu được nhiều ánh sáng và dữ liệu hơn. Về mặt video, Galaxy
-                S23 Ultra cung cấp khả năng ổn định video được cải thiện trong video và khả năng quay video hiện lên tới
-                8K ở tốc độ 30 khung hình/giây (tăng từ 24 khung hình/giây) với góc quay rộng hơn. Ngoài ra, đừng quên
-                trải nghiệm Expert RAW, ứng dụng chụp ảnh nâng cao đến từ Samsung dành cho các mẫu điện thoại Galaxy cao
-                cấp để nâng cao chất lượng ảnh đáng kể.
+                <div className={cx('detail-info')} style={{ height: checkFit ? '300px' : 'fit-content' }}>
+                    Galaxy S23 Ultra mang đến một thiết kế quen thuộc, nhìn thoáng qua, trông giống hệt với người tiền
+                    nhiệm của nó: thiết kế Infinity-O Edge góc cạnh được làm cong về hai cạnh bên, mặt lưng hoàn thiện
+                    từ chất liệu kính nhám mờ. Ngoài ra, thiết bị vẫn đi kèm bút S Pen quen thuộc và máy ảnh zoom quang
+                    10X. Tuy nhiên phiên bản S23 Ultra vẫn có những điểm khác biệt như viền 2 bên được vát phẳng hơn,
+                    kèm theo cụm camera chính sẽ to hơn một chút với vòng kim loại bao quanh camera nhìn rõ nét hơn. Màn
+                    hình ít bo cong hơn 30%, đem lại trải nghiệm dùng bút S Pen tốt hơn. <br />
+                    <br /> Ngoài ra, phiên bản S23 Ultra nói riêng và dòng Galaxy S23 nói chung có thiết kế đối xứng đậm
+                    chất Ultra và thân thiện với môi trường hơn, qua việc 12 linh kiện làm từ nguyên vật liệu tái chế
+                    bao gồm nhôm, thủy tinh tái chế tiền tiêu thụ và nhựa tổng hợp từ lưới đánh cá, thùng nước và chai
+                    nhựa polyetylen terephthalate (PET) đã qua sử dụng. Thế hệ S23 cũng là thiết bị di động đầu tiên sử
+                    dụng mặt kính Corning Gorilla Glass Victus 2 chống chịu tốt hơn khi rơi trên các bề mặt cứng và
+                    thiết kế kết hợp linh kiện làm từ vật liệu tái chế tiền tiêu thụ lên tới 22%. Bao bì của thiết bị
+                    cũng được làm 100% từ vật liệu tái chế. Galaxy S23 Ultra - Thiết kế hướng đến sự phát triển bền vững
+                    Hệ thống camera siêu phân giải và bắt trọn mọi khoảnh khắc dù ngày hay đêm S23 Ultra có màn hình QHD
+                    OLED 6,8 inch với độ phân giải 3088 x 1400 pixel và độ sáng tối đa 1.750 nits, tốc độ làm mới 120Hz
+                    giống như trước đây, có thể giảm xuống 1Hz. <br />
+                    <br />
+                    Tuy nhiên màn hình mới đã được thiết kế sao cho phẳng hơn, vừa đảm bảo khả năng trình chiếu vô cực
+                    lại khiến cho hai cạnh của máy được dày lên, giúp sử dụng S Pen dễ dàng hơn. Galaxy S23 Ultra còn hỗ
+                    trợ chế độ chụp ảnh thiên văn sử dụng Hyperlapse. Hệ thống camera chính gồm 5 ống kính kèm cảm biến
+                    lấy nét laser và đèn flash LED vẫn được đặt phía bên trái điện thoại. Công nghệ AI xác định chủ thể
+                    (object-based AI) sẽ phân tích từng chi tiết trong khung hình, thậm chí từng đặc điểm nhỏ trên khuôn
+                    mặt như tóc, mắt, để phản ánh chân thực đặc điểm của đối tượng trong ảnh... Camera chính có độ phân
+                    giải tăng vọt lên 200MP cùng ống kính khẩu độ lớn F/1.7. Bộ ba camera còn lại phía sau vẫn không
+                    thay đổi: một camera góc siêu rộng 12MP, cộng với một camera tele 3x và một camera tele 10x, cả hai
+                    đều có độ phân giải 10MP. <br />
+                    <br />
+                    Camera selfie hiện cũng là 12MP sau khi giảm từ 40MP trong S22 Ultra. Ngoài ra, Galaxy S23 Ultra có
+                    thể điều chỉnh độ sáng một cách tự động suốt cả ngày. Camera Mắt Thần Bóng Đêm (Night Ography
+                    Camera) mới giảm flare sáng hiệu quả trên S23 giúp bắt được những điểm ảnh thu sáng và ổn định khung
+                    hình video. Sâu hơn, Galaxy S23 Ultra giúp bắt được những khung hình và thước phim sắc nét nhờ việc
+                    được xây dựng với điểm ảnh 2,4um – lớn nhất từ ​​trước đến nay của Samsung, cho phép các ống kính
+                    máy ảnh thu được nhiều ánh sáng và dữ liệu hơn. Về mặt video, Galaxy S23 Ultra cung cấp khả năng ổn
+                    định video được cải thiện trong video và khả năng quay video hiện lên tới 8K ở tốc độ 30 khung
+                    hình/giây (tăng từ 24 khung hình/giây) với góc quay rộng hơn. Ngoài ra, đừng quên trải nghiệm Expert
+                    RAW, ứng dụng chụp ảnh nâng cao đến từ Samsung dành cho các mẫu điện thoại Galaxy cao cấp để nâng
+                    cao chất lượng ảnh đáng kể.
+                    {checkFit && (
+                        <div className={cx('shadow')} onClick={handleFit}>
+                            <div className={cx('show-more')}>Xem thêm &darr;</div>
+                        </div>
+                    )}
+                </div>
+
+                {/* <div className={cx('compare')}>
+                    <div style={{ display: 'flex' }}>
+                        <p>So sánh sản phẩm tương tự</p>
+                        <div style={{ position: 'relative' }}>
+                            <input
+                                placeholder="Bạn cần tìm sản phẩm nào..."
+                                type="text"
+                                style={{
+                                    width: 700,
+                                    height: 40,
+                                    borderRadius: 15,
+                                    outline: 'none',
+                                    border: 'none',
+                                    padding: '5px 10px',
+                                    boxShadow: '0 4px 6px #ccc',
+                                }}
+                            />
+                            <FaSearch
+                                style={{ padding: 5, position: 'absolute', right: 5, top: '20%', fontSize: '25px' }}
+                            />
+                        </div>
+                    </div>
+                </div> */}
             </div>
-            </div>
-            
         </>
     );
 }
